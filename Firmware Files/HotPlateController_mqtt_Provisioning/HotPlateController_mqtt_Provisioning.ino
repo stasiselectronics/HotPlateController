c1fd3d7c4100eb1ec850d8ef54a1bd81e8809e4e
Chandler McCowan <csmccowa@ncsu.edu>
2021-07-11 16:03:07 +0200

    separate files for readability

--- Firmware Files/HotPlateController_mqtt_Provisioning/HotPlateController_mqtt_Provisioning.ino
+++ Firmware Files/HotPlateController_mqtt_Provisioning/HotPlateController_mqtt_Provisioning.ino
@@ -29,56 +29,6 @@ char msg_buffer[TOTAL_MEMORY];
 char mySSID[BUFFSIZE];
 char myPassword[BUFFSIZE];
 
-String readstring(unsigned int timeout){
-  char message_buffer[BUFFSIZE] = "Uninitialized";
-  memset(message_buffer, '\0', BUFFSIZE);
-  unsigned int start_time = millis();
-  bool done = false;
-  unsigned int counter = 0;
-  while(!done){
-    if(Serial.available()){
-      message_buffer[counter]=Serial.read();
-      if(message_buffer[counter] == '\n'){
-        done = true;
-        message_buffer[counter] = '\0';
-        // replace line feed with null terminating
-      }
-      counter++;
-    }
-    if(counter>BUFFSIZE){
-      counter=0;
-      memset(message_buffer, '\0', BUFFSIZE); // clear the buffer
-    }
-    if(millis() - start_time > timeout){
-      done = true;
-      memset(message_buffer, '\0', BUFFSIZE); // clear the buffer
-      strcpy(message_buffer, "Timeout");
-    }
-  }
-  String myresponse = (String) message_buffer;
-  return myresponse;
-}
-
-unsigned int readNetworkInfo(){
-  unsigned int checksum = 0;
-  for(int i = 0; i < 64; i++){
-    mySSID[i]=(char)EEPROM.read(i);
-    myPassword[i]=(char)EEPROM.read(i+64);
-    checksum+=(byte)mySSID[i] + (byte)myPassword[i];
-  }
-  return checksum;
-}
-
-unsigned int saveNetworkInfo(){
-  unsigned int checksum = 0;
-  for(int i = 0; i < 64; i++){
-    EEPROM.write(i, mySSID[i]);
-    EEPROM.write(i+64, myPassword[i]);
-    checksum+=(byte)mySSID[i] + (byte)myPassword[i];
-  }
-  EEPROM.commit();
-  return checksum;
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -95,69 +45,8 @@ void setup() {
   // Setup Memory
   EEPROM.begin(EEPROM_SIZE);
 
-  Serial.println("Checking for existing network credentials");
-  if(readNetworkInfo()){
-    // there are existing crednetials, lets try those 
-    Serial.println("Credentials found:");
-    Serial.println(mySSID);
-    Serial.println(myPassword); 
-    Serial.print("Checksum:  ");Serial.println(readNetworkInfo());
-    Serial.println("Let's try connecting to that one");
-  }
-  else{
-    Serial.println("Going to go check what WiFi Networks are there...\n");
-    int number_of_networks = WiFi.scanNetworks();
-    if (number_of_networks == 0) {
-      Serial.println("Oh dear! No networks found...");
-      // TODO: add error handling
-    }
-    else{
-      Serial.println("There are " + (String) number_of_networks + " networks available to connect to");
-      Serial.flush();
-      delay(10);
-      for (int i = 0; i < number_of_networks; ++i) {
-        // Print SSID and RSSI for each network found
-        Serial.print(i+1); Serial.print(" : ");
-        String mySSID = WiFi.SSID(i);
-        Serial.print(mySSID);
-        Serial.print('\n');
-        Serial.flush();
-        delay(3);
-      }
-      // ask which one to connect to
-      Serial.println("What's the SSID you want to connect to? Enter its number and press enter");
-      Serial.println("If nothing happens, make sure you are appending a line feed to your input");
-      String response = readstring(RESPONSE_TIMEOUT);
-      if(response == "Timeout"){
-        Serial.println("Input timeout, restart to try again");
-        while(true){
-          int i = rand()*100;
-        }
-      }
-      response.toCharArray(mySSID, BUFFSIZE);
-      Serial.println("Connecting to " + (String)mySSID);
-      Serial.println("What's the password?");
-      response = readstring(RESPONSE_TIMEOUT);
-      response.toCharArray(myPassword, BUFFSIZE);
-    }
-  }
-  // Either we loaded credentials or we asked for new ones
-  // ready to try and connect
-  Serial.println("Attempting to connect");
-  WiFi.begin((char*)mySSID, (char*)myPassword);
-  unsigned long start_time = millis();
-  while(WiFi.status() != WL_CONNECTED) {
-    unsigned long current_time = millis();
-    if(current_time-start_time>=1000) {
-      Serial.print("\u2592"); // Print a block ▒
-      start_time=current_time;
-    }
-  }
-  Serial.println("\n\nDevice connected successfully!");
-  Serial.print("IPv4 Address:  ");Serial.println(WiFi.localIP());
+  WiFiProvision();
   
-  Serial.println("Saving credentials");
-  Serial.print("Checksum:  ");Serial.println(saveNetworkInfo());
 }
 
 
---
+++ Firmware Files/HotPlateController_mqtt_Provisioning/Serial_IO.ino
@@ -0,0 +1,29 @@
+String readstring(unsigned int timeout){
+  char message_buffer[BUFFSIZE] = "Uninitialized";
+  memset(message_buffer, '\0', BUFFSIZE);
+  unsigned int start_time = millis();
+  bool done = false;
+  unsigned int counter = 0;
+  while(!done){
+    if(Serial.available()){
+      message_buffer[counter]=Serial.read();
+      if(message_buffer[counter] == '\n'){
+        done = true;
+        message_buffer[counter] = '\0';
+        // replace line feed with null terminating
+      }
+      counter++;
+    }
+    if(counter>BUFFSIZE){
+      counter=0;
+      memset(message_buffer, '\0', BUFFSIZE); // clear the buffer
+    }
+    if(millis() - start_time > timeout){
+      done = true;
+      memset(message_buffer, '\0', BUFFSIZE); // clear the buffer
+      strcpy(message_buffer, "Timeout");
+    }
+  }
+  String myresponse = (String) message_buffer;
+  return myresponse;
+}
